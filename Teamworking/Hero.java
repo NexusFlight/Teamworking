b75341699cde54a1a5ba85b88113ce705979ef45
Astrotag <36883641+Astrotag@users.noreply.github.com>
2019-06-06 11:41:55 +0100

    Update Hero.java
    Commented

--- Teamworking/Hero.java
+++ Teamworking/Hero.java
@@ -3,8 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Hero here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Team A
  */
 public class Hero extends Actor
 {
@@ -32,7 +31,7 @@ public class Hero extends Actor
         img.scale(25, 26);
         atkImg.scale(50,25);
         setImage(img);
-    }
+    }//end Hero
 
     /**
      * Act - do whatever the Hero wants to do. This method is called whenever
@@ -45,40 +44,42 @@ public class Hero extends Actor
         updatePosition();
         move(); 
         attack();
-    }  
+    }//end act
     public void setArmor(){
         hasArmor = true;
         getWorld().addObject(new Message("You now have Armor!!",this), x, y - 20);
         heroHealth = 150;
-    }
+    }//end setArmour
     
     private void checkCollison(){
         int prevX = x;
         int prevY = y;
         if(isTouching(Wall.class)){
             setLocation(prevX,prevY);  
-        }
+        }//end if
         if(isTouching(Portal.class)){
             Portal portal = (Portal)getOneIntersectingObject(Portal.class);
             portal.transitionWorld(this);
-        }
+        }//end if
         if(isTouching(Potion.class) && !timerRunning){
             Potion potion = (Potion)getOneIntersectingObject(Potion.class);
             int addHP = potion.getHP();
             if(addHP + heroHealth > 100 && !hasArmor){
                 heroHealth = 100;
-            }else if(addHP + heroHealth > 150 && hasArmor){
+            }//end elseif
+            else if(addHP + heroHealth > 150 && hasArmor){
                 heroHealth = 150;
-            }else{
+            }//end else if
+            else{
                 heroHealth += addHP;
-            }
-        }
+            }//end else
+        }//end if
         if(isTouching(ItemCrate.class)){
             ItemCrate crate = (ItemCrate)getOneIntersectingObject(ItemCrate.class);
             if(timerRunning){
                 crate.destroyCrate(this);
-            }
-        }
+            }//end if
+        }//end if
         if(isTouching(Enemy.class)){
             enemy = (Enemy)getOneIntersectingObject(Enemy.class);
             if(timerRunning)
@@ -89,7 +90,7 @@ public class Hero extends Actor
                     enemy.takeDamage(heroDamage,this);
                 }
                 hasTakenDamage = true;
-            }
+            }//end if
             else
             {
                 if(damageRecovery < 1)
@@ -101,57 +102,60 @@ public class Hero extends Actor
                     if(x < mobX)
                     {//knockback statements for the player.
                         setLocation(mobX-35,y);
-                    }
+                    }//end 
                     else
                     {
                         setLocation(mobX+35,y);
                     }
                     damageRecovery = 30;
-                }
-            }
-        }
+                }//end if
+            }//end else
+        }//end if
         if(!timerRunning && enemy != null)
         {
             enemy.setMoveDir(2);
-        }
-    }
+        }//end if
+    }//end checkCollision
 
     private void updatePosition(){
         this.x = getX();
         this.y = getY();
-    }
+    }//end updatePosition
 
     private void flipImage(){
         img.mirrorHorizontally();
         setImage(img);
-    }
+    }//end flipImage
 
     private void flipImage(GreenfootImage image)
     {
         image.mirrorHorizontally();
         setImage(image);
-    }
+    }//end flipImage
 
     private void move(){
         if(Greenfoot.isKeyDown("w") || Greenfoot.isKeyDown("up")){
             setLocation(x,y-speed);
-        }else if(Greenfoot.isKeyDown("s") || Greenfoot.isKeyDown("down")){
+        }//end if
+        else if(Greenfoot.isKeyDown("s") || Greenfoot.isKeyDown("down")){
             setLocation(x,y+speed);
-        }else if(Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")){
+        }//end else if
+        else if(Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")){
             setLocation(x-speed,y);
             if(!facingLeft){
                 flipImage();
                 facingLeft = !facingLeft;
-            }
-        }else if(Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")){
+            }//end if
+        }//end else if
+        else if(Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")){
             setLocation(x+speed,y);
             if(facingLeft){
                 flipImage();
                 facingLeft = !facingLeft;
                 lastKey = "d";
-            }
-        }
-    }
+            }//end if
+        }//end else if
+    }//end move
 
     private void attack()
     {   //Rusty sword = 5 damage
@@ -164,22 +168,22 @@ public class Hero extends Actor
                 flipImage(atkImg);
                 atkLeft = true;
                 setLocation(x+25,y);
-            }
+            }//end if
             else if(facingLeft && atkLeft)
             {
                 flipImage(atkImg);
                 atkLeft = false;
                 setLocation(x-25,y);
-            }
+            }//end if
             else
             {
                 setImage(atkImg);
-            }
-        }
+            }//end else
+        }//end if
         if(timerRunning)
         {
             timer--;  
-        } 
+        } //end if
         if(timer == 0)
         {
             setLocation(x,y);
@@ -190,25 +194,25 @@ public class Hero extends Actor
             hasTakenDamage = false;
             timer = 20;
         }
-    }
+    }//end if
 
     public void takeDamage(int damage)
     {   //user mobDamag as its parameter which is deducted from player health
         heroHealth -= damage;
-    }
+    }//end takeDamage
 
     public int getHeroX()
     {
         return getX();
-    }
+    }//end getHeroX
 
     public int getHeroY()
     {
         return getY();
-    }
+    }//end getHeroY
 
     public int getHeroDamage()
     {
         return heroDamage;
-    }
-}
+    }//end getHeroDamage
+}//end class
