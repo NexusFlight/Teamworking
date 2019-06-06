f68670ecdfcc63d48b92d176def639aafbd0a585
Astrotag <36883641+Astrotag@users.noreply.github.com>
2019-06-06 11:47:57 +0100

    Update Portal.java
    Commented

--- Teamworking/Portal.java
+++ Teamworking/Portal.java
@@ -2,10 +2,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Portal here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Team A
  */
 public class Portal extends Actor
 {
@@ -16,42 +13,42 @@ public class Portal extends Actor
     World5 world5;
     public Portal(World1 world){
         world1 = world;
-    }
+    }//end Portal 
 
     public Portal(World2 world){
         world2 = world;
-    }
+    }//end Portal 
 
     public Portal(World3 world){
         world3 = world;
-    }
+    }//end Portal 
 
     public Portal(World4 world){
         world4 = world;
-    }
+    }//end Portal 
 
     public Portal(World5 world){
         world5 = world;
-    }   
+    }//end Portal 
 
     public void transitionWorld(Hero hero){
         if(world1 != null){
             Greenfoot.setWorld(world1);
             world1.setHero(hero);
-        }
+        }//end if
         if(world2 != null){
             Greenfoot.setWorld(world2);
             world2.setHero(hero);
-        }
+        }//end if
         if(world3 != null){
             Greenfoot.setWorld(world3);
             world3.setHero(hero);
-        }
+        }//end if
         if(world4 != null){
             Greenfoot.setWorld(world4);
-        }
+        }//end if
         if(world5 != null){
             Greenfoot.setWorld(world5);
-        }
-    }          
-}
+        }//end if
+    }//end transitionWorld
+}//end class
