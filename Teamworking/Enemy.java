0307fe07d423fd06bbf03ee6cea4f099018b3b46
Astrotag <36883641+Astrotag@users.noreply.github.com>
2019-06-06 11:34:32 +0100

    Update Enemy.java
    Commented

--- Teamworking/Enemy.java
+++ Teamworking/Enemy.java
@@ -28,24 +28,24 @@ public class Enemy extends Actor
             img.scale(18,21);
         }else{
             img.scale(24,21);
-        }
+        }//end else
         setImage(img);
         this.moveDir = moveDir;
         health = (int)(((mobType+1)*5)*worldNum/1.5);
         damage = (int)(((mobType+1)*2)*worldNum);
-    }
+    }//end Enemy constructor
 
     public void act() 
     {
         updatePosition();
         checkCollison();
         followPlayer();
-    }    
+    }//end act
 
     private void updatePosition(){
         this.x = getX();
         this.y = getY();
-    }
+    }//end updatePosition
 
     private void checkCollison()
     {
@@ -55,13 +55,14 @@ public class Enemy extends Actor
         {
             canFollow = false;
             setLocation(prevX,prevY-1);
-        }
+        }//end if
         else
         {
             canFollow = true;
-        }
-    }
+        }//end else
+    }//end checkCollision
 
+//A function to have the enemies follow the player
     private void followPlayer()
     {
         if(canFollow == true){
@@ -73,29 +74,30 @@ public class Enemy extends Actor
                     turnTowards(hero.getHeroX(), hero.getHeroY());
                     move(2);
                     setRotation(0);
-                }
-            }
-        }
-    }
+                }//end if
+            }//end if
+        }//end if
+    }//end followPlayer
 
+//A function to check if the enemy needs to change it's movement direction
     private void reverseMovement(){
         if(moveDir == 1)
         {
             moveDir = 2;
-        }
+        }//end if
         else if(moveDir == 2)
         {
             moveDir = 1;
-        }
+        }//end if
         else if(moveDir == 3)
         {
             moveDir = 4;
-        }
+        }//end if
         else if(moveDir == 4)
         {
             moveDir = 3;
-        }
-    }
+        }//end if
+    }//end reverseMovement
 
     public void move(){
         x = getX();
@@ -103,25 +105,25 @@ public class Enemy extends Actor
         if(moveDir == 1)
         {
             setLocation(x,y-moveSpd);
-        }
+        }//end if
         else if(moveDir == 2)
         {
             setLocation(x,y+moveSpd);
-        }
+        }//end else if
         else if(moveDir == 3)
         {
             setLocation(x-moveSpd,y);
-        }
+        }//end else if
         else if(moveDir == 4)
         {
             setLocation(x+moveSpd,y);
-        }
-    }
+        }//end else if
+    }//end move 
 
     public void setMoveDir(int move)
     {
         this.moveSpd = move;
-    }
+    }//end setMoveDir
 
     public void takeDamage(int damage, Hero hero){
         health -= damage;
@@ -129,29 +131,29 @@ public class Enemy extends Actor
         if(x < heroX)
         {
             setLocation(x-25,y);
-        }
+        }//end if
         else
         {
             setLocation(x+25,y);
-        }
+        }//end else
         if(health == 0)
         {
-            
+            //This would be how an enemy would be killed
         }
     }
 
     public int getDamage()
     {
         return damage;
-    }
+    }//end getDamage
 
     public int getEnemyX()
     {
         return getX();
-    }
+    }//end getEnemyX
 
     public int getEnemyY()
     {
         return getY();
-    }
-}
+    }//end getEnemyY
+}//end Enemy class
