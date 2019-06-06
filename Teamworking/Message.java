5bcc14a0d5b3d63d352d400a51fde9f94c687950
Astrotag <36883641+Astrotag@users.noreply.github.com>
2019-06-06 11:46:19 +0100

    Update Message.java
    Commented

--- Teamworking/Message.java
+++ Teamworking/Message.java
@@ -1,10 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Message here.
- * 
  * @author (your name) 
- * @version (a version number or a date)
  */
 public class Message extends Actor
 {
@@ -14,17 +11,14 @@ public class Message extends Actor
         this.hero = hero;
         GreenfootImage img = new GreenfootImage(message, 15, Color.WHITE, Color.BLACK);
         setImage(img);
-    }
-    /**
-     * Act - do whatever the Message wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    }//end Message class
+
     public void act() 
     {
         setLocation(hero.getHeroX(), hero.getHeroY()-20);
         timer--;
         if(timer <= 0){
             getWorld().removeObject(this);
-        }
-    }    
-}
+        }//end if
+    } //end act
+}//end class
